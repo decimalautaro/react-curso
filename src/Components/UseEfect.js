68f963308054dad5ebfb418a6d1940b0773ba7bc
Lautaro Decima <decimalautaro@gmail.com>
2023-01-10 15:07:09 -0300

    practica con useEfect

--- src/Components/UseEfect.js
+++ src/Components/UseEfect.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 export const UseEfect = () => {
   const [usuario, setUsuario] = useState("lautaro");
   const [fecha, setFecha] = useState("03-11-1994");
+  const [contador, setContador] = useState(0);
 
   const cambiarFecha = () => {
     setFecha(new Date().toLocaleDateString());
@@ -17,12 +18,16 @@ export const UseEfect = () => {
   }, []);
 
   useEffect(() => {
-    console.log("se a cambiado el usuario!!");
+    console.log("se a modificado el usuario: " + contador);
+
+    setContador(contador + 1);
   }, [usuario]);
   return (
     <div>
       <h1>UseEfect</h1>
-      <strong className="label">{usuario}</strong>
+      <strong className={contador > 10 ? "label label-green" : "label"}>
+        {usuario}
+      </strong>
       <strong className="label">{fecha}</strong>
 
       <p>
