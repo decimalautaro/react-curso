a7928d0038d9b26913c91df40db564265437d14d
Lautaro Decima <decimalautaro@gmail.com>
2023-01-10 13:53:29 -0300

    usando useEfect

--- src/Components/UseEfect.js
+++ src/Components/UseEfect.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const UseEfect = () => {
-  return <div>UseEfect</div>;
+  const [usuario, setUsuario] = useState("lautaro");
+  const [fecha, setFecha] = useState("03-11-1994");
+
+  const cambiarFecha = () => {
+    setFecha(new Date().toLocaleDateString());
+  };
+
+  const modificarUser = (e) => {
+    setUsuario(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log("Se ha cargado el componente de UseEfect!");
+  }, []);
+
+  useEffect(() => {
+    console.log("se a cambiado el usuario!!");
+  }, [usuario]);
+  return (
+    <div>
+      <h1>UseEfect</h1>
+      <strong className="label">{usuario}</strong>
+      <strong className="label">{fecha}</strong>
+
+      <p>
+        <input
+          type="text"
+          onChange={modificarUser}
+          placeholder="cambiar nombre"
+        />
+        <button onClick={cambiarFecha}>Cambiar fecha</button>
+      </p>
+    </div>
+  );
 };
