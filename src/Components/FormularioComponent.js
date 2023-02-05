6b6845a59ca8284b7c2d512a56d4742e87aff5a5
Lautaro Decima <decimalautaro@gmail.com>
2023-02-05 00:55:19 -0300

    feat: formularios y onSubmit

--- src/Components/FormularioComponent.js
+++ src/Components/FormularioComponent.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const FormularioComponent = () => {
+
+    const [usuario,setUsuario] = useState ({})
+
+    const conseguirDatoForm = e =>{
+        e.preventDefault();
+
+        let datos = e.target;
+        let usuario = {
+            nombre: datos.nombre.value,
+            apellido:datos.apellido.value,
+            genero: datos.genero.value,
+            biografia:datos.biografia.value
+        }
+        console.log(usuario)
+        setUsuario(usuario)
+    }
     return (
         <>
             <h1>Formularios con react</h1>
-            <form>
-                <input type="text" placeholder='Nombre' />
-                <input type="text" placeholder='Apellido' />
-                <select>
+
+            { usuario.biografia && usuario.biografia.length >= 1 &&
+            ( <div className='infoUser label '>
+            {usuario.nombre} {usuario.apellido} es un {usuario.genero} y
+            su biografia es: <p>{usuario.biografia}</p>
+
+        </div>)
+            }
+           
+
+            <form onSubmit={conseguirDatoForm}>
+                <input name='nombre' type="text" placeholder='Nombre' />
+                <input name='apellido' type="text" placeholder='Apellido' />
+                <select name='genero'>
                     <option value="hombre">Hombre</option>
                     <option value="mujer">Mujer</option>
                 </select>
-                <textarea placeholder='Biografia'></textarea>
+                <textarea name='biografia' placeholder='Biografia'></textarea>
                 <input type="submit" value="Enviar" />
 
             </form>
