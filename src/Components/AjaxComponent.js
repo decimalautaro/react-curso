e97fb545c18feb9d50d88899cd017e7f284e9dd0
Lautaro Decima <decimalautaro@gmail.com>
2023-02-04 23:39:08 -0300

    feat: peticiones ajax con fetch usando useEffect

--- src/Components/AjaxComponent.js
+++ src/Components/AjaxComponent.js
@@ -2,19 +2,58 @@ import React, { useEffect, useState } from "react";
 
 export const AjaxComponent = () => {
   const [usuarios, setUsuarios] = useState([]);
-  const getUsuariosEstaticos = () => {
-    setUsuarios([
-      {
-        id: 1,
-        nombre: "lautaro",
-        apellido: "decima",
-      },
-    ]);
-  };
+  // const getUsuariosEstaticos = () => {
+  //   setUsuarios([
+  //     {
+  //       id: 1,
+  //       nombre: "lautaro",
+  //       apellido: "decima",
+  //     },
+  //     {
+  //       id: 2,
+  //       nombre: "andres",
+  //       apellido: "murgo",
+  //     },
+  //     {
+  //       id: 3,
+  //       nombre: "emanuel",
+  //       apellido: "cortinez",
+  //     },
+  //     {
+  //       id: 4,
+  //       nombre: "alejandro",
+  //       apellido: "renna",
+  //     },
+  //     {
+  //       id: 5,
+  //       nombre: "Juan",
+  //       apellido: "de los santos",
+  //     },
+  //   ]);
+  // };
+
+  const getUsuariosAjaxPms = () =>{
+    fetch ("https://reqres.in/api/users?page1")
+    .then((respuesta) => respuesta.json())
+    .then(
+      
+      (resultadoFinal) =>{
+        setUsuarios(resultadoFinal.data);
+        console.log(usuarios)
+    },
+    error =>{
+        console.log(error);
+    })
+  }
 
   useEffect(() => {
-    getUsuariosEstaticos();
-  }, []);
+   
+ 
+    // getUsuariosEstaticos();
+    getUsuariosAjaxPms();
+    
+
+  },[]);
   return (
     <div>
       <h2>Listado de usuarios via AJAX</h2>
@@ -24,7 +63,7 @@ export const AjaxComponent = () => {
           return (
             <li key={usuario.id}>
               {" "}
-              {usuario.nombre} {usuario.apellido}{" "}
+              {usuario.first_name} {usuario.last_name}{" "}
             </li>
           );
         })}
